665a930dc5e074db069c7541e3c4f662ba33a70c
superior <superior.ppro@gmail.com>
2024-02-12 18:56:04 -0500

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/BillBoard.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/BillBoard.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class BillBoard : MonoBehaviour
@@ -8,5 +7,12 @@ public class BillBoard : MonoBehaviour
     void LateUpdate()
     {
         transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,Camera.main.transform.rotation* Vector3.up);
+    }
+    private void Start()
+    {
+        if(transform.root.GetComponent<PhotonView>().IsMine)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
---
+++ BGN CITY WARS/Assets/NetWorkPlayerSync.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using Photon.Pun;
+using RootMotion.FinalIK;
+
+public class NetWorkPlayerSync : MonoBehaviour
+{
+
+    private PhotonView PV;
+    private PhotonSerializerBGN PunSerializer;
+    private PlayerActionsVar playerActions;
+    [SerializeField]
+    private GameObject[] NotMine;
+    [SerializeField]
+    private Component[] NotMineComponents;
+    private LookAtIK lookik;
+    private AimIK aimik;
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        PunSerializer = GetComponent<PhotonSerializerBGN>();
+        aimik = GetComponent<AimIK>();
+        lookik = GetComponent<LookAtIK>();
+        playerActions = GetComponent<PlayerActionsVar>();
+
+        if(!PV.IsMine)
+        {
+            #region Disable all not mine objects
+            foreach (GameObject item in NotMine)
+            {
+                item.SetActive(false);
+            }
+            #endregion
+            #region Disable all not mine Components
+            foreach (Component item in NotMineComponents)
+            {
+                if (item is Behaviour)
+                {
+                    ((Behaviour)item).enabled = false;
+                }
+            }
+            #endregion
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region Set variables anyway
+        PunSerializer.LookIK = (int)lookik.solver.IKPositionWeight;
+        PunSerializer.AimIK = (int)aimik.solver.IKPositionWeight;
+        #endregion
+
+        if (PV.IsMine)
+        {
+            //   PunSerializer.SkinID =
+            PunSerializer.InventoryTrack = playerActions.InventoryTrack;
+            PunSerializer.CurrentWeaponType = playerActions.Weapontype;
+            PunSerializer.Fired = playerActions.Fired;
+
+
+        }
+        else
+        {
+            aimik.solver.IKPositionWeight = PunSerializer.AimIK;
+
+            lookik.solver.IKPositionWeight = PunSerializer.LookIK;
+    
+
+        }
+
+    }
+}
