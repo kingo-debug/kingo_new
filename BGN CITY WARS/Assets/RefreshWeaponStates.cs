513d27857f1ec3d78df15866235617e32d61b6d9
superior <superior.ppro@gmail.com>
2024-02-17 20:10:02 -0500

    updated weapon shop systen and switched to simplelit for skins

--- BGN CITY WARS/Assets/RefreshWeaponStates.cs
+++ BGN CITY WARS/Assets/RefreshWeaponStates.cs
@@ -1,30 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class RefreshWeaponStates : MonoBehaviour
 
 {
     public GameObject EquippedItem;
-    public string EquipCategory;
-    private InitiateData data;
-    private void OnDisable()
-    {
-        SaveEquipped();
 
-    }
-  
-    public void RefreshAllWeaponStates()
-    {
-        SaveEquipped();
-    
-    }
 
-    public void SaveEquipped()
-    {
-        ES3.Save(EquipCategory, EquippedItem);
-     
-   
-    }
    
 }
