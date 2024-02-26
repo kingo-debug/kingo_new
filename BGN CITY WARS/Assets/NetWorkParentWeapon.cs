c33d0954ac466d5f8af1f5e9576762c2c7043a49
superior <superior.ppro@gmail.com>
2024-02-25 20:26:09 -0500

    save

--- BGN CITY WARS/Assets/NetWorkParentWeapon.cs
+++ BGN CITY WARS/Assets/NetWorkParentWeapon.cs
@@ -3,15 +3,16 @@ using Photon.Pun;
 public class NetWorkParentWeapon : MonoBehaviour
 {
     private PhotonView PV;
-    private WeaponType weapontype;
+    [SerializeField]
+    private int InventorySlot;
+
     // Start is called before the first frame update
     void Start()
     {
         PV = GetComponent<PhotonView>();
-        weapontype = GetComponent<WeaponType>();
         if (transform.parent == null && PV.IsMine==false)
         {
-            transform.parent = GameObject.Find(PV.Owner.ToString()).transform.GetChild(0).GetComponent<InventoryManager>().Inventory[weapontype.Weapontype].transform;
+            transform.parent = GameObject.Find(PV.Owner.ToString()).transform.GetChild(0).GetComponent<InventoryManager>().Inventory[InventorySlot].transform;
 
         }
     }
