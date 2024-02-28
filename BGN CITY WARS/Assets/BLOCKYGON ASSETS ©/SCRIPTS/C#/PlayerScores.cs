8903a797956d73cdfdf81c9e860a85229a4059a2
superior <superior.ppro@gmail.com>
2024-02-28 11:22:19 -0500

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/PlayerScores.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/PlayerScores.cs
@@ -1,14 +1,28 @@
 
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
 public class PlayerScores : MonoBehaviourPunCallbacks, IPunObservable
 {
     [SerializeField]
     private new PhotonView photonView;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI NickName;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI KillCount;
 
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
+        if (photonView.IsMine)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+
+            FindParentItem();
+        }
+        UpdateScoreData();
     }
 
     [Header("Player Room Status")]
@@ -26,14 +40,70 @@ public class PlayerScores : MonoBehaviourPunCallbacks, IPunObservable
             stream.SendNext(TotalRoomKills);   //room kills Track
             Debug.Log("after stream.SendNext(PlayerName +  stream.SendNext(TotalRoomKills");
         }
-        else if  (stream.IsReading) // is Reading(otherplayers)
-        { 
+        else if (stream.IsReading) // is Reading(otherplayers)
+        {
             Debug.Log("before TotalRoomKills = (int)stream.ReceiveNext" + "PlayerName = (string)stream.ReceiveNext");
             PlayerName = (string)stream.ReceiveNext();
             TotalRoomKills = (int)stream.ReceiveNext(); // total room kills
             Debug.Log("TotalRoomKills = (int)stream.ReceiveNext" + "PlayerName = (string)stream.ReceiveNext");
         }
 
+    }
+
+
+
+
+
+
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        UpdateScoreData();
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        UpdateScoreData();
+    }
+
+
+
+    private void FindParentItem()
+    {
+        if (transform.parent == null)
+        {
+            transform.parent = GameObject.Find("SCENE Canvas").transform.GetChild(0).GetChild(1).GetChild(0);
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+
+
+    }
+
+
+
+
+
+
+
+    public void UpdateScoreData()
+    {
+
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (photonView.IsMine)
+            {
+                NickName.text = player.GetComponent<PhotonSerializerBGN>().PlayerNickName;
+                gameObject.name = KillCount.text = player.GetComponent<PlayerActionsVar>().TotalRoomkillsTrack.ToString();
+                
+            }
+            else
+            {
+                NickName.text = player.GetComponent<PhotonSerializerBGN>().PlayerNickName;
+                gameObject.name = KillCount.text = TotalRoomKills.ToString();
+             
+            }
+
+        }
     }
 
 }
