c2368ab8ef594630e1bd87eab7528ce7c94e8e10
superior <superior.ppro@gmail.com>
2024-03-26 10:20:42 -0400

    added reticle back

--- BGN CITY WARS/Assets/WeaponRecoil.cs
+++ BGN CITY WARS/Assets/WeaponRecoil.cs
@@ -1,13 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class WeaponRecoil : MonoBehaviour
 {
     // Start is called before the first frame update
+    private WeaponType weapontype;
+    private Transform Reticle;
+    public Vector2 DefaultReticleSize;
+    private void OnEnable()
+    {
+        weapontype = GetComponent<WeaponType>();
+        Reticle = GameObject.Find("CROSSHAIRS").transform.GetChild(weapontype.ReticleType).GetChild(0);
+        Reticle.GetComponent<RectTransform>().sizeDelta = DefaultReticleSize;
+    }
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
