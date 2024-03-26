8decdb14140921cc0918499c886faa4371213bee
superior <superior.ppro@gmail.com>
2024-03-26 10:00:48 -0400

    save

---
+++ BGN CITY WARS/Assets/SmoothRotate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SmoothRotate : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 RotateDirection;
+    [SerializeField]
+    private float Speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateDirection, Speed*Time.deltaTime);
+       
+    }
+}
