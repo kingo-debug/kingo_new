c2d83566fbba3caed0bb31c7edc9a5da01178cac
superior <superior.ppro@gmail.com>
2024-04-04 16:52:21 -0400

    sav

--- BGN CITY WARS/Assets/WeaponRecoil.cs
+++ BGN CITY WARS/Assets/WeaponRecoil.cs
@@ -11,11 +11,9 @@ public class WeaponRecoil : MonoBehaviour
     public float ReticleRecoilAmount;
     private RectTransform recttransform;
     public float RecoilCoolDownSpeed;
-    public float SpreadSpeed;
     public bool PlayerAiming = false;
     public float ReticleMaxSpread;
 
-    public float DebugRectmag;
     private void OnEnable()
     {
         weapontype = GetComponent<WeaponType>();
@@ -30,8 +28,6 @@ public class WeaponRecoil : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        DebugRectmag = recttransform.sizeDelta.x;
-    
             if (recttransform.sizeDelta.x != DefaultReticleSize&& !PlayerAiming)
         {
             recttransform.sizeDelta -= new Vector2(RecoilCoolDownSpeed, RecoilCoolDownSpeed) ;
@@ -44,11 +40,20 @@ public class WeaponRecoil : MonoBehaviour
             {
                 recttransform.sizeDelta = new Vector2(DefaultReticleSize, DefaultReticleSize);
             }
-        }
-     else if(PlayerAiming && recttransform.sizeDelta.magnitude < AimingReticleSize )
+        } //DEFAULT STATE
+            else if(recttransform.sizeDelta.x != AimingReticleSize && PlayerAiming)
         {
-             recttransform.sizeDelta += new Vector2(RecoilCoolDownSpeed, RecoilCoolDownSpeed) * Time.deltaTime;
-        }
+            recttransform.sizeDelta -= new Vector2(RecoilCoolDownSpeed, RecoilCoolDownSpeed);
+
+            if (recttransform.sizeDelta.x > ReticleMaxSpread) // Limit Max
+            {
+                recttransform.sizeDelta = new Vector2(ReticleMaxSpread, ReticleMaxSpread);
+            }
+            else if (recttransform.sizeDelta.x < DefaultReticleSize) // LimitMin)
+            {
+                recttransform.sizeDelta = new Vector2(AimingReticleSize, AimingReticleSize);
+            }
+        }  // AIMING STATE
     }
     public void AddReticleRecoid()
     {
