14b0791139cdbf2c87bb094a8964ccd3b9d9ebe1
superior <superior.ppro@gmail.com>
2024-04-26 23:08:54 -0400

    sav

--- BGN CITY WARS/Assets/CarPlayerEntry.cs
+++ BGN CITY WARS/Assets/CarPlayerEntry.cs
@@ -1,11 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
 public class CarPlayerEntry : MonoBehaviour
 {
+
+    private PhotonView PlayerPV;
+    [SerializeField]
+    private GameObject DoorUIButton;
  void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name + "InRange");
+        if(other.CompareTag("Player"))
+        {
+            PlayerPV = other.GetComponent<PhotonView>();
+            if(PlayerPV.IsMine)
+            {
+                DoorUIButton = PlayerPV.transform.Find("PLAYER Canvas").transform.Find("CF2-Rig").transform.Find("CarDoor").gameObject;
+                DoorUIButton.SetActive(true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        Debug.Log(other.name + "InRange");
+        if (other.CompareTag("Player"))
+        {
+            PlayerPV = other.GetComponent<PhotonView>();
+            if (PlayerPV.IsMine)
+            {
+                DoorUIButton.SetActive(false);
+            }
+        }
     }
 }
