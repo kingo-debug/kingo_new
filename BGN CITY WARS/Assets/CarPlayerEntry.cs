66258a6777a4b2a3cc4beba59bf97484c60b4ebb
superior <superior.ppro@gmail.com>
2024-04-28 22:03:42 -0400

    save

--- BGN CITY WARS/Assets/CarPlayerEntry.cs
+++ BGN CITY WARS/Assets/CarPlayerEntry.cs
@@ -6,13 +6,15 @@ public class CarPlayerEntry : MonoBehaviour
     private PhotonView PlayerPV;
     [SerializeField]
     private GameObject DoorUIButton;
+    public GameObject Player;
  void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.name + "InRange");
         if(other.CompareTag("Player"))
         {
             PlayerPV = other.GetComponent<PhotonView>();
-            if(PlayerPV.IsMine)
+            Player = other.gameObject;
+            if (PlayerPV.IsMine)
             {
                 DoorUIButton = PlayerPV.gameObject.transform.Find("PLAYER Canvas").transform.Find("CF2-Rig").transform.GetChild(0).transform.Find("CarDoor").gameObject;
                 DoorUIButton.SetActive(true);
@@ -31,5 +33,14 @@ public class CarPlayerEntry : MonoBehaviour
                 DoorUIButton.SetActive(false);
             }
         }
+    }
+
+
+
+    public void EnterCar()
+    {
+        Player.transform.root.gameObject.SetActive(false); // Disable Player
+        transform.parent.GetChild(1).gameObject.SetActive(true); // Car Cameras Enable
+        transform.Find("CAR CANVAS").gameObject.SetActive(true);
     }
 }
