c7f61b171dd20a7e25cc525926c43e1948c14957
superior <superior.ppro@gmail.com>
2024-05-31 20:47:20 -0400

    save

--- BGN CITY WARS/Assets/RoomPlayerSpawn.cs
+++ BGN CITY WARS/Assets/RoomPlayerSpawn.cs
@@ -9,15 +9,21 @@ public class RoomPlayerSpawn : MonoBehaviour
     public Transform[] SpawnPoints;
     [SerializeField]
     private GameObject RoomScoreBoard;
+    private GameObject SpawnedPlayer;
     // Start is called before the first frame update
-    void Awake()
+    void Start()
         
     {
-        Transform RandomSP = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
-        PhotonNetwork.Instantiate("BGN-Player new", RandomSP.position, Quaternion.identity);
-      GameObject Scoreitem=   PhotonNetwork.Instantiate("_PLAYER SCORE BOARD ITEM_", transform.position, Quaternion.identity);
-        Scoreitem.transform.localScale =new Vector3(1, 1, 1);
+        Transform RandomSP = SpawnPoints[0];
+       
+
+        SpawnedPlayer = PhotonNetwork.Instantiate("BGN-Player new", RandomSP.position, Quaternion.identity);  // spawn player
+
+        SpawnedPlayer.transform.position = SpawnPoints[0].position;  // set player position
+      GameObject Scoreitem=   PhotonNetwork.Instantiate("_PLAYER SCORE BOARD ITEM_", transform.position, Quaternion.identity); // spawn player score board item
+
         Scoreitem.transform.parent = RoomScoreBoard.transform;
+        Scoreitem.transform.localScale =new Vector3(1, 1, 1);
     }
 
 }
