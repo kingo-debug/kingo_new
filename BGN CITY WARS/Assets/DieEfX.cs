79600d595c0f1aaaecaaa1327e8cfb22aa0b81c4
superior <superior.ppro@gmail.com>
2024-06-08 09:46:53 -0400

    save

---
+++ BGN CITY WARS/Assets/DieEfX.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Photon.Pun;
+public class DieEfX : MonoBehaviour
+
+{
+    [SerializeField]
+    private GameObject[] ObjectsToHide;
+
+
+
+    private void Awake()
+    {
+        PhotonView PV = GetComponent<PhotonView>();
+        if(PV.IsMine)
+        {
+            this.enabled = false;
+        }
+    }
+    public void HideOut()
+    {
+
+    foreach (GameObject item in ObjectsToHide)
+    {
+            item.gameObject.SetActive(false);
+    }
+    
+    }
+
+}
