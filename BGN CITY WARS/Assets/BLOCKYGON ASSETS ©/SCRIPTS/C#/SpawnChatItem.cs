ab4377f153b786e32d18a3b1776944f60786fd19
superior <superior.ppro@gmail.com>
2024-06-14 17:02:14 -0400

    UPDATED CHATTING

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/SpawnChatItem.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/SpawnChatItem.cs
@@ -32,29 +32,8 @@ public class SpawnChatItem : MonoBehaviour
                 PV.RPC("SetTXTmessage", RpcTarget.AllBuffered);
             }
             #endregion
-            #region Alert All Players
-            if(Messagesfx.GetComponent<Toggle>().isOn)
-            {
-                // Find all GameObjects with the tag "Player"
-                GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-
-                // Iterate through each player and get their PhotonView component
-                foreach (GameObject player in players)
-                {
-                    PhotonView photonView = player.GetComponent<PhotonView>();
-
-                    if (photonView != null)
-                    {
-                        photonView.RPC("PlayMessageNotification", RpcTarget.Others);
-                    }
-                    else
-                    {
-                        Debug.LogWarning("No PhotonView found on: " + player.name);
-                    }
-                }
-            }
+       
         
-            #endregion
 
         }
 
@@ -64,8 +43,8 @@ public class SpawnChatItem : MonoBehaviour
     [PunRPC]
     public void SetTXTmessage()
     {
-        MessageItem.transform.GetChild(0).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = Inputfield.GetComponent<TMP_InputField>().text;  // set chat item logic
-        MessageItem.transform.GetChild(1).transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = PhotonNetwork.NickName + " : ";  // set chatter name item logic
+        MessageItem.transform.GetChild(0).transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = Inputfield.GetComponent<TMP_InputField>().text;  // set chat item logic
+        MessageItem.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = PhotonNetwork.NickName + " : ";  // set chatter name item logic
     }
 
 }
