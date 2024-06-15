268af10dd684e8cc41006166b5dd7ad832f67a94
superior <superior.ppro@gmail.com>
2024-06-15 19:56:24 -0400

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ChatItem.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/ChatItem.cs
@@ -9,6 +9,8 @@ public class ChatItem : MonoBehaviour,IPunObservable
     private  PhotonView PV;
     private  bool Set;
 
+
+
     private void Start()
     {
         PV = GetComponent<PhotonView>();
@@ -16,8 +18,19 @@ public class ChatItem : MonoBehaviour,IPunObservable
         {
             transform.parent = GameObject.Find("ROOM DATA").transform.GetChild(1).transform.GetChild(6).transform.GetChild(0).transform.GetChild(0);
         }
-        
-     
+
+        #region Flip if Sender is Player
+        if(PV.IsMine)
+        {
+            transform.GetChild(0).localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z); // invert BGs
+            transform.GetChild(0).transform.Find("chat text").localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            transform.GetChild(1).gameObject.SetActive(false); // disable name
+            transform.GetChild(0).GetComponent<RectTransform>().localPosition = new Vector3(850f, transform.position.y, transform.position.z);
+        }
+        #endregion
+
+
+
     }
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
