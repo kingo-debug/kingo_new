edbe14be248f33be59e6bd560373a161b185424f
superior <superior.ppro@gmail.com>
2024-06-17 23:24:18 -0400

    save

--- BGN CITY WARS/Assets/ReloadAnimator.cs
+++ BGN CITY WARS/Assets/ReloadAnimator.cs
@@ -6,20 +6,26 @@ public class ReloadAnimator : MonoBehaviour
     public Transform Clip;
     public Transform Hand;
     public WeaponStatus weapon;
+    private Transform FoundWeapon;
 
 
 
     public void DetatchClip()
     {
-        Clip = weapon.CurrentWeapon.transform.GetChild(0).transform.Find("CLIP PLACE").transform.GetChild(0);
-        Clip.transform.parent = Hand;
-        Clip.transform.localPosition = new Vector3(0, 0, 0);
-        Clip.transform.localRotation = new Quaternion(0, 0, 0,0);
+  
+         
+            Clip = weapon.CurrentWeapon.transform.GetChild(0).transform.Find("CLIP PLACE").transform.GetChild(0);
+            FoundWeapon = weapon.CurrentWeapon.transform;
+            Clip.transform.parent = Hand;
+            Clip.transform.localPosition = new Vector3(0, 0, 0);
+            Clip.transform.localRotation = new Quaternion(0, 0, 0, 0);
+
+  
     }
 
     public void AttachClip()
     {
-        Clip.transform.parent = weapon.CurrentWeapon.transform.GetChild(0).transform.Find("CLIP PLACE");
+        Clip.transform.parent = FoundWeapon.GetChild(0).transform.Find("CLIP PLACE");
         Clip.transform.localPosition = new Vector3(0, 0, 0);
         Clip.transform.localRotation = new Quaternion(0, 0, 0, 0);
     }
