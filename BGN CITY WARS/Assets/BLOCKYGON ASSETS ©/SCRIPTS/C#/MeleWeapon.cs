a585ef966b42b1fd95568823650a760a81c8220a
superior <superior.ppro@gmail.com>
2024-06-28 18:16:14 -0400

    sae

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/MeleWeapon.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/MeleWeapon.cs
@@ -1,193 +1,547 @@
+using System.Collections;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 public class MeleWeapon : MonoBehaviour
 {
-  //vars
-public WeaponDATA WeaponType;
-public LayerMask layerMask;
-    [Header("Specs")]
-    public int BodyDamage = 40;
-    public int HeadDamage = 40;
-    public float SwingSize = 0.5f;
-    public float SwingSpeed = 1f;
-    private int Swings = 0;
-    public bool Fired;
 
-private float lastshot = 0f;
-public bool Canfire;
-public Transform SwingPoint;
-public AudioSource AS;
-private PhotonView PV;
-public PhotonView TPV;
-private RaycastHit hit;
-private Collider collided;
-private Transform PlayerParent;
-public int weapontype;
-
-    [Header("SFX")]
+    #region Variables
+    private WeaponStatus weaponstatus;
+    private WeaponType weapontype;
     [SerializeField]
-    private AudioClip SwingSFX;
+    private PlayerActionsVar Parentvariables;
+    [Header("Weapon Specs")]
+    [SerializeField]
+    private float PullOutTime;
+    public float SwingSpeed = 0.5f;
+    public float SwingSize = .5f;
+    public int BodyDamage;
+    public int HeadDamage;
+    public int TotalDamageDealt;
+    public float AttackRange;
+    private UpdateKillDisplay Killcountupdate;
+    private UpdateAmmoUI UpdateAmmoUI;
+    private PlayerScores ScoreItem;
+
+
+    [Space(10)]
+
+
+   
+    public int BulletsFired = 0;
+    private float lastshot = 0f;
+    [Space(10)]
+    [Header("Firing Info")]
+    public bool Canfire = false;
+    private bool started;
+    public bool Fired;
+    public float modifiedFireRate;
+    [Space(10)]
+
+
+
+    [HideInInspector]
+    public bool bodyshotHit;
+    [HideInInspector]
+    public bool headshotHit;
+    [Header("Weapon Settings")]
+    [SerializeField]
+    private LayerMask layermask;
+    private Vector3 point;
+    private Transform pos;
+    private Transform AttackPoint;
+    private RaycastHit hit;
+    private RaycastHit hit2;
+
+    [Header("Weapon Audio")]
+    private AudioSource AS;
+    [SerializeField]
+    private AudioClip FireSFX;
     [SerializeField]
     private AudioClip BodyshotSFX;
+    [SerializeField]
+    private AudioClip HeadshotSFX;
 
 
-void OnEnable() 
-{
-  weapontype=WeaponType.Weapontype;
-  }
-     void Start()
-     {
-        Invoke("FindParent", .5f);
-       PV =  this.GetComponent<PhotonView>();
-       
+    private Transform PlayerParent;
 
-      }
+ 
+    [SerializeField]
+    private GameObject CameraMain;
+    private Animator Parentanimator;
+    [Space(10)]
 
- void FindParent()
+    [SerializeField]
+    private Transform DefaultReticle;
+
+
+    //pun variables
+    [Header("Debugs")]
+    private PhotonView PV;
+    private Collider collided;
+    private GameObject KillFeed;
+    private GameObject HeadShotKill;
+    private int TargetHP = 100;
+    private int TargetShield;
+    private PhotonView TPV;
+    private bool hasExecutedKill = false;
+    public float DamageDelay;
+    private int LastDamageType;
+    private GameObject HitReticleCrosshair;
+    private TextMeshProUGUI AmmoMessage;
+    private SwimPlayerControl swimcontrol;
+    private MainCharacterController mainCharacterController;
+
+    #endregion Variables
+
+    private void OnEnable()
     {
-        PlayerParent = transform.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent;
-        SwingPoint = PlayerParent.transform.Find("Points").transform.Find("Attack Point");
-    }
-    void Update()
+        StartCoroutine(ReadyForFire());
+        #region CrosshairSetUp
+        weapontype = GetComponent<WeaponType>();
+        DefaultReticle = GameObject.Find("CROSSHAIRS").transform.GetChild(weapontype.ReticleType);
+        HitReticleCrosshair = DefaultReticle.transform.GetChild(1).gameObject;
+        #endregion
 
-     {
-        if(transform.parent!= null)
+
+        AS = GetComponent<AudioSource>();
+
+        #region find  and assign kill pop up feeds.
+        KillFeed = GameObject.Find("KILL FEEDS").transform.GetChild(0).gameObject;
+        HeadShotKill = GameObject.Find("KILL FEEDS").transform.GetChild(1).gameObject;
+
+        #endregion
+        Invoke("FindParent", .15f);
+        PV = this.GetComponent<PhotonView>();
+        if (!PV.IsMine)
         {
-       PlayerParent.GetComponent<PlayerActionsVar>().Fired = Fired;
-       Canfire =  PlayerParent.GetComponent<PlayerActionsVar>().canfire;
+            GetComponent<MeleWeapon>().enabled = false;
         }
 
-        if (Time.time > lastshot + 0.2f)
-         {     
-         Fired = false;     
-         }
+        SyncFireAnim();
+        collided = hit.collider;
 
 
 
-         if(Canfire)
-         { //canfire
+
+  
+
+    }
+    private void OnDisable()
+    {
+        bodyshotHit = false;
+        headshotHit = false;
+        Parentanimator.SetBool("RELOAD", false);
+        AmmoMessage.text = ("");
+        Canfire = false;
 
 
-          if (ControlFreak2.CF2Input.GetMouseButton(0) && PV.IsMine & Time.time > lastshot+SwingSpeed)
-           {
-            AS.PlayOneShot(SwingSFX, 1f);
-            Fired = true;
-            Swing();             
+
+    }
+    void FindParent()
+    {
+        PlayerParent = transform.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent.parent;
+        weaponstatus = PlayerParent.GetComponent<WeaponStatus>();
+        Parentvariables = PlayerParent.GetComponent<PlayerActionsVar>();
+        Parentvariables.Fired = Fired;
+        Parentanimator = PlayerParent.GetComponent<Animator>();
+        swimcontrol = PlayerParent.GetComponent<SwimPlayerControl>();
+
+        mainCharacterController = PlayerParent.GetComponent<MainCharacterController>();
+        ScoreItem = PlayerParent.GetComponent<PlayerActionsVar>().ScoreItemUI.gameObject.GetComponent<PlayerScores>();
+    }
+    private void Start()
+    {
+        DamageDelay = 0.25f;
+        TargetHP = 100;
+        AttackPoint = GameObject.Find("Pointst").transform.Find("Attack Point");
+        CameraMain = Camera.main.gameObject; pos = CameraMain.transform.GetChild(2);
+        SyncFireAnim();
+
+        Killcountupdate = GameObject.Find("KILL COUNT TEXT DISPLAY").GetComponent<UpdateKillDisplay>();
+     
+
+   
+
+
+    }
+
+    void Update()
+    {//update S
+     //var sync with plaayer
+
+
+        modifiedFireRate = 1.0f / SwingSpeed;
+
+        if (TargetShield < 1 && TotalDamageDealt >= 100)//ResetTotalDamage
+        {
+
+            TotalDamageDealt = 0;
+
+        }
+
+        if (TargetHP <= 0 && !hasExecutedKill && PV.IsMine)
+        {
+            if (LastDamageType == 0)
+            {
+                Kill();
+                hasExecutedKill = true; // Set the flag to indicate the code has executed
             }
+            else if (LastDamageType == 1)
+            {
+                HeadKill();
+                hasExecutedKill = true; // Set the flag to indicate the code has executed
+            }
+        }
 
-{
-  return;
-}
+
+        if (Time.time > lastshot + 0.2f)
+        {
+            Fired = false;
+            Parentvariables.Fired = false;
+            Parentanimator.SetBool("shoot", false);
+
+        }
+
+
+        if (Canfire)
+        { //canfire
+
+            if (ControlFreak2.CF2Input.GetMouseButton(0) == true) // first condition
+
+            {
+                if (PV.IsMine && Time.time > lastshot + modifiedFireRate &&  Canfire && !swimcontrol.Swiming && !mainCharacterController.Rolling)
+                {
+                    AS.PlayOneShot(FireSFX, 1f);
+
+                    float nextActionTime = 0.0f;
+                    float period = 5f;
+                    if (Time.time > nextActionTime)
+                    {
+                        nextActionTime += period;
+                        Attack();
+                    }
+                } // other conditions
+
+            }
 
         }//canfire
 
-    void Swing()
-     {
 
-     //track shots fired
-     Swings = Swings+1;
-     //Reset FireRate
-     lastshot = Time.time;
+        else
+        {
+            return;
+        }
+
+
+    }//update E
+
+
+    void Attack()
+
+    {
+        #region AtShootActions !!!! ^
+        started = true;
+        Fired = true;
+        Parentvariables.Fired = true;
+        Parentanimator.SetBool("shoot", true);
+        #endregion
+
+
+
+
+
+
+        lastshot = Time.time;
+
      
-     
-       //fire
-     if (Physics.SphereCast(SwingPoint.position,SwingSize,SwingPoint.forward,out hit,1,layerMask))
-     {
-      collided = hit.collider;
-     Debug.Log(hit.collider.name,hit.collider);
 
-     
-     BodyShot();
-
-     }
-
-   
-}
-
- void BodyShot()
+        // Fire RayCast//
+        Physics.SphereCast(AttackPoint.position,SwingSize, PlayerParent.forward, out hit, AttackRange, layermask);
 
 
-    { // SF
+
+        collided = hit.collider;
+
+        point = (hit.point);
+        started = false;
 
 
-        if (collided != null & collided.name == "HIT BOX-BODY")
 
+        if (collided == null || collided.gameObject.layer == 0)
+
+        { return; }
+        else
 
         {
-          Debug.Log("reached Bodyshot();");
-          
-            if (TPV != null)
-            
-            
-              //self shoot detect
-            if (TPV.IsMine)
-            return;
-            else // other online player detect
-            {
-                AS.PlayOneShot(BodyshotSFX, 1f);
+            TPV = collided.transform.root.transform.GetChild(0).GetComponentInParent<PhotonView>();
 
-                PV.RPC("Bodydamage", RpcTarget.Others);
+       
+            //Call Methods
 
-                //  TPV = collided.GetComponent<PhotonView>();
+            BodyShot();
 
-                Debug.Log("Real Player Detected-Body");
-
-            }
-
-            else if (collided.name == "HIT BOX-BODY" & TPV == null)
-            {
-               Debug.Log("reached before tag();");
-                      ///AI detct
-            if(collided.CompareTag("AI"))
-             
-            {
-            TakeDamage takedamage = collided.transform.parent.GetComponent<TakeDamage>();
-
-                AS.PlayOneShot(BodyshotSFX, 1f);
-
-                Debug.Log("AI Target Detected-Body");
-
-             
-               takedamage.Takedamage(BodyDamage);
-
-            }
-
-            else
-             {
-
-             AS.PlayOneShot(BodyshotSFX, 1f);
-
-                Debug.Log("Iron Target Detected-Body");
-
-            }
-              
-            }
+            HeadShot();
 
         }
 
-        else return;
+        //check Bodyshot
 
-    } //EF
-
-
+        void BodyShot()
 
 
+        { // SF
+
+
+            if (collided != null && collided.name == "HIT BOX-BODY")
+
+
+            {
+
+                if (TPV != null)
+                    //self shoot detect
+                    if (TPV.IsMine && TPV.gameObject.tag != ("CAR"))
+                        return;
+                    else // other online player detect
+                    {
+                        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+                        if (TargetHP > 0)
+                        {
+                            AS.PlayOneShot(BodyshotSFX, 1f);
+
+                            RpcTarget RPCTYPE = new RpcTarget();
+                            if (TPV.IsMine && TPV.gameObject.tag == ("CAR"))
+                            {
+                                RPCTYPE = RpcTarget.All;
+                            }
+                            else RPCTYPE = RpcTarget.Others;
+
+                            Bodydamage();
+
+                            Debug.Log("Real Player Detected-Body");
+
+                            HitReticleCrosshair.SetActive(true);
+                        }
+
+                    }
 
 
 
+                else if (collided.name == "HIT BOX-BODY" && TPV == null)
+                {
+                    ///AI detct
+                    if (collided.CompareTag("AI"))
+
+                    {
+                        TakeDamage takedamage = collided.transform.GetComponentInParent<TakeDamage>();
+
+                        AS.PlayOneShot(BodyshotSFX, 1f);
+
+                        Debug.Log("AI Target Detected-Body");
+
+                        //Hit Reticle Enable
+                        HitReticleCrosshair.SetActive(true);
+                        takedamage.Takedamage(BodyDamage);
+
+                    }
+
+                    else
+                    {
+
+                        AS.PlayOneShot(BodyshotSFX, 1f);
+
+                        Debug.Log("Iron Target Detected-Body");
+
+                        TakeDamage takedamage = collided.transform.parent.GetComponent<TakeDamage>();
+                        if (takedamage != null)
+                        { takedamage.Takedamage(BodyDamage); }
+
+                        //Hit Reticle Enable
+                        HitReticleCrosshair.SetActive(true);
+
+
+                    }
+
+                }
+
+            }
+
+            else return;
+
+        } //EF
+          //check headshot
+
+        void HeadShot()
+
+
+        { // SF
+
+
+            if (collided != null && collided.name == "HIT BOX-HEAD")
+
+
+            {
+
+                if (TPV != null)
+                    //self shoot detect
+                    if (TPV.IsMine && TPV.gameObject.tag != ("CAR"))
+                        return;
+                    else // other online player detect
+                    {
+
+
+                        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+                        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
+                        if (TargetHP > 0)
+                        {
+                            AS.PlayOneShot(HeadshotSFX, 1f);
+
+                            RpcTarget RPCTYPE = new RpcTarget();
+                            if (TPV.IsMine && TPV.gameObject.tag == ("CAR"))
+                            {
+                                RPCTYPE = RpcTarget.All;
+                            }
+                            else RPCTYPE = RpcTarget.Others;
+
+                            Headdamage();
+
+                            //  TPV = collided.GetComponent<PhotonView>();
+
+                            Debug.Log("Real Player Detected-HEAD");
+
+                            //Hit Reticle Enable
+                            HitReticleCrosshair.SetActive(true);
+                        }
+
+                    }
+
+
+
+                else if (collided.name == "HIT BOX-HEAD" && TPV == null)
+                {
+                    ///AI detct
+                    if (collided.CompareTag("AI"))
+
+                    {
+                        TakeDamage takedamage = collided.transform.GetComponentInParent<TakeDamage>();
+
+                        AS.PlayOneShot(HeadshotSFX, 1f);
+
+                        Debug.Log("AI Target Detected-HEAD");
+
+                        //Hit Reticle Enable
+                        HitReticleCrosshair.SetActive(true);
+                        takedamage.Takedamage(HeadDamage);
+
+                    }
+
+                    else
+                    {
+
+                        AS.PlayOneShot(HeadshotSFX, 2.5f);
+
+                        Debug.Log("Iron Target Detected-hEAD");
+
+                        TakeDamage takedamage = collided.transform.parent.GetComponent<TakeDamage>();
+                        if (takedamage != null)
+                        { takedamage.Takedamage(HeadDamage); }
+
+                        //Hit Reticle Enable
+                        HitReticleCrosshair.SetActive(true);
+
+
+                    }
+
+                }
+
+            }
+
+            else return;
+
+        } //EF
+
+    }//EF
+
+
+    void Bodydamage()
+    {
+        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
+        LastDamageType = 0;
+
+        if (TargetHP > 0)
+        {
+            hasExecutedKill = false;
+            TPV.RPC("Takedamage", RpcTarget.All, BodyDamage);
+            StartCoroutine(UpdateTargetHP());
+
+        }
+
+        Debug.Log("body reached");
+        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
+    }
+
+    void Kill()
+    {
+        KillFeed.gameObject.SetActive(true);
+        Parentvariables.TotalRoomkillsTrack++;
+        Killcountupdate.UpdateKillCount(Parentvariables.TotalRoomkillsTrack);
+        ScoreItem.UpdateScoreData();
+        TotalDamageDealt = 0;
+
+
+        TargetHP = 100;
+
+        GameObject Killpopupitem = PhotonNetwork.Instantiate("KILLS POPUP ITEM", transform.position, Quaternion.identity); // spawn kill UI notification
+        Killpopupitem.GetComponent<KillPopupManager>().PlayerKilled = TPV.GetComponent<PhotonSerializerBGN>().PlayerNickName;
+        Killpopupitem.GetComponent<KillPopupManager>().PlayerKiller = PhotonNetwork.NickName;
+        hasExecutedKill = false;
+    }
+
+    void HeadKill()
+    {
+        HeadShotKill.gameObject.SetActive(true);
+        Parentvariables.TotalRoomkillsTrack++;
+        Killcountupdate.UpdateKillCount(Parentvariables.TotalRoomkillsTrack);
+        ScoreItem.UpdateScoreData();
+        TotalDamageDealt = 0;
+
+
+        TargetHP = 100;
+
+        GameObject Killpopupitem = PhotonNetwork.Instantiate("KILLS POPUP ITEM", transform.position, Quaternion.identity); // spawn kill UI notification
+        Killpopupitem.GetComponent<KillPopupManager>().PlayerKilled = TPV.GetComponent<PhotonSerializerBGN>().PlayerNickName;
+        Killpopupitem.GetComponent<KillPopupManager>().PlayerKiller = PhotonNetwork.NickName;
+        hasExecutedKill = false;
+    }
+
+    void Headdamage()
+    {
+        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
+        LastDamageType = 1;
+        if (TargetHP > 0)
+        {
+            hasExecutedKill = false;
+            TPV.RPC("Takedamage", RpcTarget.All, HeadDamage);
+            StartCoroutine(UpdateTargetHP());
+
+        }
+
+        Debug.Log("Head reached");
+        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
     }
 
 
 
 
-
-
-
-
-
-
-
-    }//END CLASS
+    void SyncFireAnim()
+    {
+        Parentanimator.SetFloat("FireRate", SwingSpeed / 3.5f);
+    }
+    #region /////Coroutines/////
 
 
 
@@ -196,5 +550,33 @@ void OnEnable()
 
 
 
+    IEnumerator UpdateTargetHP()
+    {
+        yield return new WaitForSeconds(DamageDelay);
+        TargetHP = TPV.GetComponent<TakeDamage>().HP;
+        TargetShield = TPV.GetComponent<TakeDamage>().Shield;
+
+        if (TargetShield <= 0)
+        {
+            TotalDamageDealt += BodyDamage;
+        }
+
+        if (TargetHP <= 0)
+        {
+            TotalDamageDealt = 0;
+        }
+
+    }
+    IEnumerator ReadyForFire()
+    {
+        yield return new WaitForSeconds(PullOutTime);
+        Canfire = true;
+    #endregion
+}
+
+
+
+
+}//ef
 
 
