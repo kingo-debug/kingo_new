8a18429512117a454817dffa5eb598464b5d641d
superior <82778809+kingo-debug@users.noreply.github.com>
2024-08-29 22:45:26 -0400

    mini map + main map add

--- BGN CITY WARS/Assets/AddOtherEnable.cs
+++ BGN CITY WARS/Assets/AddOtherEnable.cs
@@ -4,17 +4,17 @@ public class AddOtherEnable : MonoBehaviour
 {
     private Enableothers enableothers;
     private DisableOthers disableothers;
-    [SerializeField] private string  FindTargetObject;
+    [SerializeField] private string FindTargetObject;
 
     void Start()
     {
-        // Find the Enableothers component on the "MINI MAP UI" GameObject
+        // Find the Enableothers and DisableOthers components on the target GameObject
         enableothers = GameObject.Find(FindTargetObject).GetComponent<Enableothers>();
-        disableothers= GameObject.Find(FindTargetObject).GetComponent<DisableOthers>();
+        disableothers = GameObject.Find(FindTargetObject).GetComponent<DisableOthers>();
 
         // Add this object's Transform to the Enableothers.Others array
         AddToOthersArrayEnable(transform);
-        // Add this object's Transform to the Enableothers.Others array
+        // Add this object's Transform to the DisableOthers.Others array
         AddToOthersArrayDisable(transform);
     }
 
@@ -42,9 +42,6 @@ public class AddOtherEnable : MonoBehaviour
         enableothers.Others = newArray;
     }
 
-
-
-
     private void AddToOthersArrayDisable(Transform newTransform)
     {
         // Check if the array is null or empty, initialize it if necessary
@@ -67,5 +64,48 @@ public class AddOtherEnable : MonoBehaviour
 
         // Step 4: Replace the old array with the new array
         disableothers.Others = newArray;
+    }
+
+    private void OnDestroy()
+    {
+        // Remove this object's Transform from the Enableothers.Others array
+        RemoveFromOthersArray(enableothers, transform);
+
+        // Remove this object's Transform from the DisableOthers.Others array
+        RemoveFromOthersArray(disableothers, transform);
+    }
+
+    private void RemoveFromOthersArray(object othersComponent, Transform toRemove)
+    {
+        if (othersComponent is Enableothers enableOthersComponent && enableOthersComponent.Others != null)
+        {
+            enableOthersComponent.Others = RemoveTransformFromArray(enableOthersComponent.Others, toRemove);
+        }
+        else if (othersComponent is DisableOthers disableOthersComponent && disableOthersComponent.Others != null)
+        {
+            disableOthersComponent.Others = RemoveTransformFromArray(disableOthersComponent.Others, toRemove);
+        }
+    }
+
+    private Transform[] RemoveTransformFromArray(Transform[] array, Transform toRemove)
+    {
+        // Step 1: Create a new array with one less slot than the current array
+        Transform[] newArray = new Transform[array.Length - 1];
+
+        int newIndex = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            // Copy all elements except the one to remove
+            if (array[i] != toRemove)
+            {
+                if (newIndex < newArray.Length) // Prevent going out of bounds
+                {
+                    newArray[newIndex] = array[i];
+                    newIndex++;
+                }
+            }
+        }
+
+        return newArray;
     }
 }
