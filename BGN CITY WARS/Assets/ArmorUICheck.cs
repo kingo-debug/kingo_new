e8c6ce2d28a98ed4b3932eec9962eb20404b1d69
superior <82778809+kingo-debug@users.noreply.github.com>
2024-09-30 20:25:38 -0400

    save

--- BGN CITY WARS/Assets/ArmorUICheck.cs
+++ BGN CITY WARS/Assets/ArmorUICheck.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ArmorUICheck : MonoBehaviour
 {
     // Start is called before the first frame update
-    private CanvasGroup group;
+ [SerializeField] private CanvasGroup group;
 
  public void ShowShieldBar()
     {
