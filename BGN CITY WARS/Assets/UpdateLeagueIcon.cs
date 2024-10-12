6c1727f35f1da28cb82e90fa2fdc312e6b3bda81
superior <82778809+kingo-debug@users.noreply.github.com>
2024-10-12 00:30:22 -0400

    SAVE

--- BGN CITY WARS/Assets/UpdateLeagueIcon.cs
+++ BGN CITY WARS/Assets/UpdateLeagueIcon.cs
@@ -12,24 +12,7 @@ public class UpdateLeagueIcon : MonoBehaviour
         PV = transform.root.GetComponent<PhotonView>();
         if (PV!= null&& !PV.IsMine)   // load for others
         {
-            LeagueName = serializerBGN.LeagueLogoPath;
-            // Load all sprites from the sprite sheet
-            Sprite[] sprites = Resources.LoadAll<Sprite>("league ranking badges");
-
-            // Find the specific sprite by name
-            Sprite specificSprite = System.Array.Find(sprites, sprite => sprite.name == LeagueName);
-
-            // Check if the sprite was found
-            if (specificSprite != null)
-            {
-                // Set the sprite to a SpriteRenderer (or use it as needed)
-                GetComponent<Image>().sprite = specificSprite;
-            }
-
-            else
-            {
-                Debug.LogError("Sprite not found in the sprite sheet!");
-            }
+            Invoke("LoadOtherPlayerLeague", 0.25f);
         }
         else
         {
@@ -61,15 +44,31 @@ public class UpdateLeagueIcon : MonoBehaviour
         
         } // load for player menu
 
-
-
-
-
-
-
     }
 
-    
 
-    
+    void LoadOtherPlayerLeague()
+    {
+        LeagueName = serializerBGN.LeagueLogoPath;
+        // Load all sprites from the sprite sheet
+        Sprite[] sprites = Resources.LoadAll<Sprite>("league ranking badges");
+
+        // Find the specific sprite by name
+        Sprite specificSprite = System.Array.Find(sprites, sprite => sprite.name == LeagueName);
+
+        // Check if the sprite was found
+        if (specificSprite != null)
+        {
+            // Set the sprite to a SpriteRenderer (or use it as needed)
+            GetComponent<Image>().sprite = specificSprite;
+        }
+
+        else
+        {
+            Debug.LogError("Sprite not found in the sprite sheet!");
+        }
+    }
+
+
+
 }
