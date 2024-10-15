a5f1cdf97696ef9bfa137d86ea7a787d006640fa
superior <82778809+kingo-debug@users.noreply.github.com>
2024-10-15 18:44:05 -0400

    save

--- BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/BuySkin.cs
+++ BGN CITY WARS/Assets/BLOCKYGON ASSETS ©/SCRIPTS/C#/BuySkin.cs
@@ -1,72 +1,94 @@
 using UnityEngine;
 using TMPro;
+
 public class BuySkin : MonoBehaviour
 {
-
     private OwnedShopItems shopitems;
-    private GameObject BuySuccessMessage;
-    private GameObject BuyFailedMessage;
-
-    private GameObject EquipButton;
+    private GameObject buySuccessMessage;
+    private GameObject buyFailedMessage;
+    private GameObject equipButton;
 
     [SerializeField]
-    private AudioClip BuySuccessSFX;
+    private AudioClip buySuccessSFX;
     [SerializeField]
-    private AudioClip BuyFailedSFX;
+    private AudioClip buyFailedSFX;
 
-    private AudioSource AS;
-
+    private AudioSource audioSource;
     public string SkinID;
-    private StatusLoad UIStatus;
+    private StatusLoad uiStatus;
+
+    // Prices for gold and cash
     [SerializeField]
-    private int Price;
+    private int goldPrice;
+    [SerializeField]
+    private int cashPrice;
+
     private void OnEnable()
     {
         shopitems = GameObject.Find("OWNED SHOP ITEMS").GetComponent<OwnedShopItems>();
-        EquipButton = transform.parent.GetChild(1).gameObject;
-        //check if already owned
+        equipButton = transform.parent.GetChild(1).gameObject;
+
+        // Check if already owned
         if (shopitems.OwnedSkins.Contains(SkinID))
         {
-            EquipButton.SetActive(true); gameObject.SetActive(false);
+            equipButton.SetActive(true);
+            gameObject.SetActive(false);
         }
-
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        UIStatus = GameObject.Find("quick info").GetComponent<StatusLoad>();
-        AS = GameObject.Find("SFX").GetComponent<AudioSource>();
+        uiStatus = GameObject.Find("quick info").GetComponent<StatusLoad>();
+        audioSource = GameObject.Find("SFX").GetComponent<AudioSource>();
 
-        BuySuccessMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(0).gameObject;
-        BuyFailedMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(1).gameObject;
-        TextMeshProUGUI PriceText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-        PriceText.text = Price.ToString();
-      
+        buySuccessMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(0).gameObject;
+        buyFailedMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(1).gameObject;
 
+        // Display the price: prefer GoldPrice if non-zero, otherwise display CashPrice
+        TextMeshProUGUI priceText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        priceText.text = (goldPrice > 0 ? goldPrice.ToString() : cashPrice.ToString());
     }
 
     public void Buy()
     {
+        // Determine which price and currency to use
+        int priceToUse = goldPrice > 0 ? goldPrice : cashPrice;
+        string currencyKey = goldPrice > 0 ? "BGNCoins" : "BGNCash";
 
-        if (ES3.Load<int>("BGNCoins") >= Price && !shopitems.OwnedSkins.Contains(SkinID))
+        // Check if the player has enough of the selected currency and doesn't already own the skin
+        if ((goldPrice > 0 && ES3.Load<int>("BGNCoins") >= goldPrice) ||
+            (cashPrice > 0 && ES3.Load<int>("BGNCash") >= cashPrice) &&
+            !shopitems.OwnedSkins.Contains(SkinID))
         {
-            BuySuccessMessage.SetActive(true);
-            int SubtractedPrice = ES3.Load<int>("BGNCoins") - +Price;
-            ES3.Save<int>("BGNCoins", SubtractedPrice);
-            AS.PlayOneShot(BuySuccessSFX);
+            // Success purchase
+            buySuccessMessage.SetActive(true);
+            audioSource.PlayOneShot(buySuccessSFX);
+
+            // Deduct the appropriate currency
+            if (goldPrice > 0)
+            {
+                int newGoldBalance = ES3.Load<int>("BGNCoins") - goldPrice;
+                ES3.Save<int>("BGNCoins", newGoldBalance);
+            }
+            else
+            {
+                int newCashBalance = ES3.Load<int>("BGNCash") - cashPrice;
+                ES3.Save<int>("BGNCash", newCashBalance);
+            }
+
+            // Add the skin to owned items, enable equip button, and save
             shopitems.OwnedSkins.Add(SkinID);
-            EquipButton.SetActive(true); gameObject.SetActive(false);
+            equipButton.SetActive(true);
+            gameObject.SetActive(false);
             shopitems.SaveSkins();
-            UIStatus.LoadStatus();
-
-
+            uiStatus.LoadStatus();
         }
         else
         {
-            BuyFailedMessage.SetActive(true);
-            AS.PlayOneShot(BuyFailedSFX);
+            // Failed purchase
+            buyFailedMessage.SetActive(true);
+            audioSource.PlayOneShot(buyFailedSFX);
         }
     }
-
 }
--- BGN CITY WARS/Assets/BuyWeapon.cs
+++ BGN CITY WARS/Assets/BuyWeapon.cs
@@ -3,60 +3,81 @@ using UnityEngine;
 
 public class BuyWeapon : MonoBehaviour
 {
-
     private OwnedShopItems shopitems;
-    private GameObject BuySuccessMessage;
-    private GameObject BuyFailedMessage;
- 
-    private GameObject EquipButton;
+    private GameObject buySuccessMessage;
+    private GameObject buyFailedMessage;
+    private GameObject equipButton;
 
     [SerializeField]
-    private AudioClip BuySuccessSFX;
+    private AudioClip buySuccessSFX;
     [SerializeField]
-    private AudioClip BuyFailedSFX;
+    private AudioClip buyFailedSFX;
 
-    private AudioSource AS;
+    private AudioSource audioSource;
 
     public string WeaponID;
-    private StatusLoad UIStatus;
+    private StatusLoad uiStatus;
 
+    // Prices for gold and cash
     [SerializeField]
-    private int Price;
+    private int goldPrice;
+    [SerializeField]
+    private int cashPrice;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-        AS = GameObject.Find("SFX").GetComponent<AudioSource>();
+        audioSource = GameObject.Find("SFX").GetComponent<AudioSource>();
         shopitems = GameObject.Find("OWNED SHOP ITEMS").GetComponent<OwnedShopItems>();
-        EquipButton = transform.parent.GetChild(1).gameObject;
-        BuySuccessMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(0).gameObject;
-        BuyFailedMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(1).gameObject;
-        UIStatus = GameObject.Find("quick info").GetComponent<StatusLoad>();
-        TextMeshProUGUI PriceText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-        PriceText.text = Price.ToString();
+        equipButton = transform.parent.GetChild(1).gameObject;
+        buySuccessMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(0).gameObject;
+        buyFailedMessage = GameObject.Find("SHOP NOTIFICATION").transform.GetChild(1).gameObject;
+        uiStatus = GameObject.Find("quick info").GetComponent<StatusLoad>();
 
+        // Display the price: prefer GoldPrice if non-zero, otherwise display CashPrice
+        TextMeshProUGUI priceText = transform.GetChild(0).GetComponent<TextMeshProUGUI>();
+        priceText.text = (goldPrice > 0 ? goldPrice.ToString() : cashPrice.ToString());
     }
 
-   public  void Buy()
+    public void Buy()
     {
+        // Decide whether to use GoldPrice or CashPrice
+        int priceToUse = goldPrice > 0 ? goldPrice : cashPrice;
+        string currencyKey = goldPrice > 0 ? "BGNCoins" : "BGNCash";
 
-        if (ES3.Load<int>("BGNCoins") >= Price && !shopitems.OwnedWeapons.Contains(WeaponID))
+        // Check if the player has enough of the selected currency and doesn't already own the weapon
+        if ((goldPrice > 0 && ES3.Load<int>("BGNCoins") >= goldPrice) ||
+            (cashPrice > 0 && ES3.Load<int>("BGNCash") >= cashPrice) &&
+            !shopitems.OwnedWeapons.Contains(WeaponID))
         {
-            BuySuccessMessage.SetActive(true);
-            int SubtractedPrice = ES3.Load<int>("BGNCoins") - +Price;
-            ES3.Save<int>("BGNCoins", SubtractedPrice);
-            AS.PlayOneShot(BuySuccessSFX);
+            // Success purchase
+            buySuccessMessage.SetActive(true);
+            audioSource.PlayOneShot(buySuccessSFX);
+
+            // Deduct the appropriate currency
+            if (goldPrice > 0)
+            {
+                int newGoldBalance = ES3.Load<int>("BGNCoins") - goldPrice;
+                ES3.Save<int>("BGNCoins", newGoldBalance);
+            }
+            else
+            {
+                int newCashBalance = ES3.Load<int>("BGNCash") - cashPrice;
+                ES3.Save<int>("BGNCash", newCashBalance);
+            }
+
+            // Add the weapon to owned items, enable equip button, and save
             shopitems.OwnedWeapons.Add(WeaponID);
-            EquipButton.SetActive(true); gameObject.SetActive(false);
+            equipButton.SetActive(true);
+            gameObject.SetActive(false);
             shopitems.SaveWeapons();
-            UIStatus.LoadStatus();
+            uiStatus.LoadStatus();
         }
         else
         {
-            BuyFailedMessage.SetActive(true);
-            AS.PlayOneShot(BuyFailedSFX);
+            // Failed purchase
+            buyFailedMessage.SetActive(true);
+            audioSource.PlayOneShot(buyFailedSFX);
         }
     }
-  
 }
